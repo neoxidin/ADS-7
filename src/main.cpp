2c4e0fa5ddd72c2030ef75aa22ad767f45e4f1b4
neoxidin <vanya.robotov@gmail.com>
2026-06-02 20:35:46 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,62 @@
-// Copyright 2022 NNTU-CS
+#include "../include/train.h"
+
+#include <fstream>
 #include <iostream>
-#include "train.h"
+#include <random>
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+  std::ofstream out("results.csv");
 
-  while (count--)
-    train.addCar(false);
+  out << "n,type,ops\n";
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::bernoulli_distribution dist(0.5);
+
+  for (int n = 2; n <= 1000; n += 10) {
+    {
+      Train train;
+
+      for (int i = 0; i < n; ++i) {
+        train.AddCar(false);
+      }
+
+      train.GetLength();
+
+      out << n << ",all_off,"
+          << train.GetOpCount() << "\n";
+    }
+
+    {
+      Train train;
+
+      for (int i = 0; i < n; ++i) {
+        train.AddCar(true);
+      }
+
+      train.GetLength();
+
+      out << n << ",all_on,"
+          << train.GetOpCount() << "\n";
+    }
+
+    {
+      Train train;
+
+      for (int i = 0; i < n; ++i) {
+        train.AddCar(dist(gen));
+      }
+
+      train.GetLength();
+
+      out << n << ",random,"
+          << train.GetOpCount() << "\n";
+    }
+  }
+
+  out.close();
+
+  std::cout << "Results saved to results.csv\n";
+
   return 0;
 }
