4086e2838369c57f9d0b861573a8d8cbef683f08
neoxidin <vanya.robotov@gmail.com>
2026-06-02 20:27:34 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,20 +1,45 @@
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TRAIN_H_
-#define INCLUDE_TRAIN_H_
+#include "train.h"
 
-class Train {
- private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
- public:
-  Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
-};
-#endif  // INCLUDE_TRAIN_H_
+Train::Train() : countOp_(0), first_(nullptr) {}
+
+Train::~Train() {
+  if (!first_) return;
+  Car* cur = first_->next;
+  while (cur != first_) {
+    Car* tmp = cur;
+    cur = cur->next;
+    delete tmp;
+  }
+  delete first_;
+}
+
+void Train::addCar(bool light) {
+  Car* car = new Car{light, nullptr, nullptr};
+  if (!first_) {
+    car->next = car;
+    car->prev = car;
+    first_ = car;
+  } else {
+    Car* last = first_->prev;
+    last->next = car;
+    car->prev  = last;
+    car->next  = first_;
+    first_->prev = car;
+  }
+}
+
+int Train::getLength() {
+  if (!first_) return 0;
+  int  len = 1;
+  Car* cur = first_->next;
+  while (cur != first_) {
+    ++countOp_;
+    ++len;
+    cur = cur->next;
+  }
+  return len;
+}
+
+int Train::getOpCount() {
+  return countOp_;
+}
